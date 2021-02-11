9d071089d4736aeaff467cb8de068945e62000a9
Scott Hoisington <hoyzington@gmail.com>
2021-02-10 20:45:44 -0500

    implement User log in

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,13 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, except: [:create]
 
   def show
-    render json: [user, user.lists], status: :accepted
+    # byebug
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      render json: { id: user.id, username: user.username, lists: user.lists}, status: :accepted
+    else
+      render json: { message: "Incorrect username and/or password" }
+    end
   end
 
   def create
@@ -30,7 +36,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def set_user
-    user = User.find(params[:id])
+    user = User.find_by(username: params[:username])
   end
 
   def user_params
